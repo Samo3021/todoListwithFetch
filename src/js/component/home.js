2d1723670e18830827ca9c9900d0c7fa6f510674
Samo3021 <samuelcampos15009@gmail.com>
2021-02-11 02:40:42 +0000

    Done

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,59 @@
 import React from "react";
-
+import shortid from "shortid";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 export function Home() {
+	//funciones para hacerlo cambios
+	const [tarea, setTarea] = React.useState("");
+	const [arrayTareas, setArrayTareas] = React.useState([]);
+	const agregarTarea = e => {
+		e.preventDefault();
+
+		setArrayTareas([
+			...arrayTareas,
+			{
+				id: shortid.generate(),
+				nombreTarea: tarea
+			}
+		]);
+		setTarea("");
+	};
+	const eliminartTarea = id => {
+		let newlist = arrayTareas.filter(item => item.id !== id);
+		setArrayTareas(newlist);
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<h1>TODOLIST</h1>
+			<div className="container justify-content">
+				<div>
+					<form onSubmit={agregarTarea}>
+						<input
+							type="text"
+							className="form-control mb-2"
+							placeholder="Ingrese Tarea"
+							onChange={e => setTarea(e.target.value)}
+							value={tarea}
+						/>
+					</form>
+				</div>
+				<div>
+					<ul className="list-group overflow-auto">
+						{arrayTareas.map(item => (
+							<li className="list-group-item" key={item.id}>
+								<span className="lead">{item.nombreTarea}</span>
+								<button
+									type="button"
+									className="btn btn-muted "
+									onClick={() => eliminartTarea(item.id)}>
+									<i className="fas fa-times-circle"></i>
+								</button>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
 //include bootstrap npm library into the bundle
 import "bootstrap";
 
-//include your index.scss file into the bundle
+//include your index.scss file into the bundles
 import "../styles/index.scss";
 
 //import your own components
